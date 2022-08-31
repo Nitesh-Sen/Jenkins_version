e13d3c7f3134f4a50ff196d8de1ad06a02cb7e55
Nitesh-Sen <109130234+Nitesh-Sen@users.noreply.github.com>
2022-08-31 23:37:19 +0530

    Update nitesh.sh

--- nitesh.sh
+++ nitesh.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 pwd
+echo pwd
 docker --version
+echo dockerversion
 docker images
+echo dockerimages
 docker ps
+echo dockerps
+pwd
+ls -lhtr
 docker build -t niteshsen/pipeline:apache3 .
 docker run -d -p 8080:80  niteshsen/pipeline:apache3
